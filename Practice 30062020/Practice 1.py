e8e9fb82de9631ccfaf8632623cb99082b1b5b93
zihaofoo <zihaofoo@mit.edu>
2020-06-30 18:08:45 +0800

    Update Practice 1.py

--- Practice 30062020/Practice 1.py
+++ Practice 30062020/Practice 1.py
@@ -1,8 +1,53 @@
-# -*- coding: utf-8 -*-
+# Comment techniques 
+# This is a single line comment.
+# Put beside a code to tell others what it is
 """
-Created on Tue Jun 30 17:38:51 2020
-
-@author: queky
+This is a multi line comment  (1)
+This is a multi line comment  (2)
+This is a multi line comment  (3)
+People start codes with multi-line comment to tell other people what the code does. 
 """
 
-print("Ying Jie is lame")
+# Variable in Python
+"""
+Apple = 5.0  --> Apple is the name of a variable.
+'=' assigns value to a variable.
+
+Dos and Don'ts
+1. Alphabets, numbers and underscores. No spaces are allowed. 
+    E.g. yingjie_45 
+2. Variable cannot start with a number.
+    E.g. 1apple = 50; (ERROR)
+    E.g. 22 = 1 (ERROR)    
+    E.g. Apple1 is okay.
+3. Variables are case sensitive.
+    E.g. As1 =/= AS1
+"""
+
+# Basic Data Types in Python
+"""
+4 basic data types
+1. Integer
+    1, 2, 3, 4 etc. No decimals
+    E.g. Var1 = 2 (integer)
+    E.g. Var1 = 2.0 (Not an integer)  
+    Typecasting: Var1 = int(2.0)    Var1 is now 2
+    
+2. Float
+    1.0, 2.661, 3.1415 (Got decimals)
+    E.g. Var1 = 3.3 (float)
+    E.g. Var1 = 3 (NOT a float)
+    Typecasting:  Var1 = float(3) (FLOAT)   Var1 is now = 3.0
+    
+3. Boolean (Logic)
+    True, False
+    E.g. Var1 = True
+    Typecasting: bool1 = bool(1)   --> True
+                 bool2 = bool(0)   --> False
+        
+4. String (Character string)
+    Enclosed within " " or ' ' 
+    E.g. str1 = "Ying jie is smart."
+    E.g. str1 = Ying Jie is smart (ERROR)    
+    Typecasting: Str1 = str('Hello')
+"""
